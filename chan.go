cff8f4194ae2108b7ffb78403f90086067b02f35
Matias Insaurralde <matias@insaurral.de>
2016-08-09 19:07:47 -0400

    Implementing a function to send C.struct_message as a Go Message

--- chan.go
+++ chan.go
@@ -1,6 +1,16 @@
 package main
 
+/*
+#include <stdlib.h>
+
+#include "common.h"
+#include "message.h"
+*/
 import "C"
+
+import (
+  "unsafe"
+)
 
 // This is an exported function. It takes a *C.Char, converts it to a Go string and sends it to the channel.
 //export publishString
@@ -8,5 +18,20 @@ func publishString(s *C.char) {
   var goString string
   goString = C.GoString(s)
   stringChannel <- goString
+  return
+}
+
+// This function provides a bridge between the C Message struct and Go. It'll send a Go Message to the channel.
+//export publishMessage
+func publishMessage(m *C.struct_Message) {
+  var data []byte = C.GoBytes(m.data, m.length)
+
+  var message Message = Message{data}
+
+  C.free(unsafe.Pointer(m.data))
+  C.free(unsafe.Pointer(m))
+
+  messageChannel <- message
+
   return
 }
